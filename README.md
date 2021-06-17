0a98098856e7cf070d5536c8c5c4417a1efc389e
unsky559 <6kostya@gmail.com>
2021-06-17 22:55:35 +0300

    init: webpack starter config

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# webpack-typesctipt-starter
+# Webpack Typesctipt starter
+
+## What`s inside?
+- eslint-plugin
+- eslint (airbnb)
+- prettier
+- css-loader
+- sass
+- - sass-loader
+- style-loader
+- typescript
+- webpack
+- - html-webpack-plugin
+- - copy-webpack-plugin
+
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLint = require('eslint-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new CopyPlugin({ patterns: [
+      { from: path.resolve(__dirname, 'src/assets'), to: "assets" }
+    ]}),
+    new ESLint()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true,
+    port: 2000,
+  },
+};
